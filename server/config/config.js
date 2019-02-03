f0814b9e69e91f60b07c08bf5e2e34886ea6cb02
Juan M <judith1912black@gmail.com>
2019-02-02 20:24:23 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -25,7 +25,7 @@ if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 }
 else {
-    urlDB = 'mongodb://cafe-user:c123456@ds031271.mlab.com:31271/cafe'; 
+    urlDB = process.env.MONGO_URI; 
 }
 
 process.env.URLDB = urlDB;
